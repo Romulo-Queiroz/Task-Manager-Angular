7a9940e8797746b5495bd8576ca48be27e60ac4c
Romulo Freitas <rfcontatosvia@gmail.com>
2023-08-12 17:08:03 -0300

    Autenticação de usuário feita

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { HttpClient} from '@angular/common/http';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
+import { AuthService } from 'auth.service';
 
 
 @Component({
@@ -17,18 +18,14 @@ export class LoginComponent {
     private http: HttpClient,
     private formBuilder: FormBuilder,
     private modalService: NgbModal,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) {
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
       password: ['', Validators.required],
       isLogged: ['', Validators.required]
     });
-    let user = {
-      username: this.loginForm.value.username,
-      password: this.loginForm.value.password,
-      isLogged: false
-    };
   }
 
   closeModal() {
@@ -41,24 +38,12 @@ export class LoginComponent {
       password: this.loginForm.value.password,
       isLogged: false
     };
-  
-    this.http.post<any>('http://localhost:5021/Authenticate', user).subscribe(
-      response => {
-        if (response && response.isLogged === true) {
-          user.isLogged = true;
-          console.log('User logged in successfully');
-          this.router.navigate(['/tasks']);
-        } 
-        else {
-          console.log('User not logged in');
-        }
-      },
-      error => {
-        console.log('HTTP request error:', error);
-        if (error.status === 400)
-          alert('Senha ou usuário inválidos');
-        else
-          alert('HTTP request error');
+
+    this.http.post('http://localhost:5021/Authenticate', user).subscribe(
+      user => {
+        console.log(user);
+        this.authService.setUser(user);
+        this.router.navigate(['/tasks']);
       }
     );
   }
