81b2d7b7703239637f020a5f0d0ffa6a63ccd0ea
Romulo Freitas <rfcontatosvia@gmail.com>
2023-12-06 16:59:06 -0300

    feat: página de adiministração com dashboard. Será melhorado nos próximos feats desse componente. Package.json atualizado para suportar chart.js

--- src/app/admin-page/admin-page.component.ts
+++ src/app/admin-page/admin-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Chartist from 'chartist';
+import { Chart } from 'chart.js/auto';
+
 
 @Component({
   selector: 'app-admin-page',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-page.component.css']
 })
 export class AdminPageComponent {
+
+   ngOnInit(): void {
+    this.createChart();
+  }
+
+  createChart() {
+    const ctx = document.getElementById('myChart') as HTMLCanvasElement;
+    const myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Categoria 1', 'Categoria 2', 'Categoria 3'],
+        datasets: [{
+          data: [30, 50, 20],
+          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
+        }]
+      }
+    });
+  }
 
 }
