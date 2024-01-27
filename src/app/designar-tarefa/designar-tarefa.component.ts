445d6b39e502a3cb8a1479c9d71cd836afafc361
Romulo Freitas <rfcontatosvia@gmail.com>
2024-01-26 22:07:01 -0300

    Refactor: alocar tarefa para usuário em pagina de administração feita com sucesso. Pendente: recarregar página após alocar tarefa.

--- src/app/designar-tarefa/designar-tarefa.component.ts
+++ src/app/designar-tarefa/designar-tarefa.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { userModel } from 'src/Models/user.model';
 import { AsignTaskService } from 'src/Services/asign-task.service';
@@ -22,10 +22,10 @@ export class DesignarTarefaComponent {
     private asignTaskService: AsignTaskService
   ) { 
     this.taskForm = this.formBuilder.group({
-      taskTitle: [''],
-      taskDescription: [''],
-      taskCategory: [''],
-      taskUser: [''],
+      taskTitle: ['', Validators.required],
+      taskDescription: ['', Validators.required],
+      taskCategory: ['', Validators.required],
+      taskUser: ['', Validators.required],
     });
   }
 
@@ -61,4 +61,27 @@ export class DesignarTarefaComponent {
     this.modalService.dismissAll();
   }
 
+  asignTAsk() {
+
+    const newTask = {
+      Title: this.taskForm.value.taskTitle,
+      Description: this.taskForm.value.taskDescription,
+      Done: false,
+      CreatedAt: new Date(),
+      CategorieTaskId: this.taskForm.value.taskCategory,
+      UserId: this.taskForm.value.taskUser
+    };
+
+    this.asignTaskByUserService.asignTaskToUser(newTask).subscribe(
+      (response) => {
+        this.modalService.dismissAll();
+        this.closeModal();
+        window.location.reload();
+      },
+      (error) => {
+        console.log(error);
+      }
+    )
+    
+  }
 }
