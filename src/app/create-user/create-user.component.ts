0cb7d6ac7c3af6432d0f85cb419301e715589d91
Romulo Freitas <rfcontatosvia@gmail.com>
2024-02-13 18:07:47 -0300

    Feat: criando usuário a partir de um usuário admin

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { UpdateCreateUsersService } from 'src/Services/update-create-user.service';
 
 @Component({
   selector: 'app-create-user',
@@ -6,5 +9,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-user.component.css']
 })
 export class CreateUserComponent {
+
+  userForm: FormGroup;
+
+  constructor(private modalService: NgbModal,
+    private updateCreateUserService: UpdateCreateUsersService,
+    private formBuilder: FormBuilder) {
+        this.userForm = this.formBuilder.group({
+          userName: ['', Validators.required],
+          password: ['', Validators.required],
+        });
+     }
+
+  ngOnInit(): void {
+  }
+
+  closeModal() {
+    this.modalService.dismissAll();
+  }
+
+  createUser() {
+    if (this.userForm.invalid) {
+      return;
+    }
+    const newUser = {
+      userName: this.userForm.value.userName,
+      password: this.userForm.value.password
+    };
+    this.updateCreateUserService.createUser(newUser).subscribe(
+      (response) => {
+        this.modalService.dismissAll();
+        this.closeModal();
+      },
+      (error: any) => {
+        console.log(error);
+        this.modalService.dismissAll();
+        this.closeModal();
+      }
+    );
+  }
 
 }
